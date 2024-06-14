ea26d872ad02f33eb08c0a08421dc66065960659
ErhanGokce <erhangokceofis@gmail.com>
2024-06-14 20:58:38 +0300

    Created Intro Overlay.

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import {App as Canvas } from './Canvas.jsx';
 import './index.css';
+import Overlay from './Overlay.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Canvas />
+    <Overlay />
   </React.StrictMode>,
 )
